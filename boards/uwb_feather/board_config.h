b8972591a4c72d84af640bed22ad494887aa2870
Jed Hodson <jedhodson@outlook.com>
2020-06-10 16:51:19 +1000

    Added support for Prototyping Corner UWB Feather

---
+++ boards/uwb_feather/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Prototyping Corner"
+#define PRODUCT_NAME "UWB Feather"
+#define VOLUME_LABEL "PROTOBOOT"
+#define INDEX_URL "https://prototypingcorner.io/l/uwb_feather"
+#define BOARD_ID "SAMD21G18A-UWB-Feather-v0"
+
+#define USB_VID 0x1209
+#define USB_PID 0x8192
+
+#define LED_PIN PIN_PB22
+
+#endif
